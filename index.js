05ae554335f8bad292efec12f228ec30d095e1bb
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-12-12 17:43:01 +0530

    some testing code removed to

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const logger = require("./startup/logging");
 const app = express();
 const port = process.env.PORT || 3001;
 
-const p = new Promise((resolve, reject) => {
-  reject(new Error("Something wrong happaned"));
-});
-
 require("./startup/db")();
 require("./startup/routes")(app);
 
